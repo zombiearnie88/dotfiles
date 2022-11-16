e2a0aebc117aacf2b1e290f9579525d7ea47fdad
arnie <duongapollo88@gmail.com>
2022-11-16 19:14:34 +0700

    close buf, wipe out buf hot-key in linux

--- nvim/after/plugin/buffdelete.rc.lua
+++ nvim/after/plugin/buffdelete.rc.lua
@@ -6,3 +6,5 @@ end
 -- mapping
 vim.api.nvim_set_keymap("n", "∑", ":Bdelete<CR>", { noremap = true, silent = true }) -- alt + w
 vim.api.nvim_set_keymap("n", "„", ":Bwipeout<CR>", { noremap = true, silent = true }) -- alt + W
+vim.api.nvim_set_keymap("n", "<A-w>", ":Bdelete<CR>", { noremap = true, silent = true }) -- alt + w
+vim.api.nvim_set_keymap("n", "<A-W>", ":Bwipeout<CR>", { noremap = true, silent = true }) -- alt + W
