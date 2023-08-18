5ec86959c3dea07c4403c69c91b7efc9b0951aeb
duongapollo <duongapollo88@gmail.com>
2023-08-18 11:20:55 +0700

    update nvim tree config

--- nvim/after/plugin/nvim-tree.rc.lua
+++ nvim/after/plugin/nvim-tree.rc.lua
@@ -1,19 +1,23 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
+	print("nvim tree not loaded")
 	return
 end
 
---print('nvim tree loaded')
+-- print("nvim tree loaded")
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-	return
-end
+-- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+-- if not config_status_ok then
+-- 	print("nvim tree config not loaded")
+-- 	return
+-- end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+-- local tree_cb = nvim_tree_config.nvim_tree_callback
 
 local function on_attach(bufnr)
 	local api = require("nvim-tree.api")
+
+	print("NvimTree on_attach")
 
 	local function opts(desc)
 		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
@@ -95,7 +99,8 @@ local function on_attach(bufnr)
 	vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
 	vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
 	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
-	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+	vim.keymap.set("n", "<C-v>", api.node.open.vertical, opts("Open: Vertical Split"))
+	vim.keymap.set("n", "<C-h>", api.node.open.horizontal, opts("Open: Horizontal Split"))
 	vim.keymap.set("n", "<C-s>", api.node.run.system, opts("Run System"))
 	vim.keymap.set("n", "n", api.fs.create, opts("Create"))
 end
@@ -149,7 +154,7 @@ nvim_tree.setup({
 		},
 	},
 	diagnostics = {
-		enable = true,
+		enable = false,
 		show_on_dirs = true,
 		icons = {
 			hint = "",
@@ -158,20 +163,20 @@ nvim_tree.setup({
 			error = "",
 		},
 	},
-	remove_keymaps = { "s" },
+	-- remove_keymaps = { "s" },
 	view = {
-		-- adaptive_size = true,
-		width = 30,
+		adaptive_size = true,
+		width = 40,
 		side = "left",
-		mappings = {
-			list = {
-				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-				{ key = "h", cb = tree_cb("close_node") },
-				{ key = "v", cb = tree_cb("vsplit") },
-				{ key = "<C-s>", action = "system_open" },
-				{ key = "n", action = "create" },
-			},
-		},
+		-- mappings = {
+		-- 	list = {
+		-- 		{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+		-- 		{ key = "h", cb = tree_cb("close_node") },
+		-- 		{ key = "<C-v>", cb = tree_cb("vsplit") },
+		-- 		{ key = "<C-s>", action = "system_open" },
+		-- 		{ key = "n", action = "create" },
+		-- 	},
+		-- },
 	},
 	filters = {
 		dotfiles = false,
