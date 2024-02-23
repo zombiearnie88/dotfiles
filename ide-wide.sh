4e2827a87b1a59e0bc8a9cb648a788c98975f159
duongapollo <duongapollo88@gmail.com>
2024-02-23 10:50:02 +0700

    split with lzgit pane

--- ide-wide.sh
+++ ide-wide.sh
@@ -2,3 +2,5 @@
 tmux split-window -v -p 25
 tmux split-window -h -p 65
 tmux split-window -h
+tmux select-pane -U
+tmux split-window -h -p 75
--- ide.sh
+++ ide.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 tmux split-window -v -p 25
 tmux split-window -h -p 50
+tmux select-pane -U
+tmux split-window -h -p 75
